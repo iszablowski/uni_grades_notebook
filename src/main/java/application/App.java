f38c481d5aa4b3ac81298d2e9e8c4b9e4d1967f6
Igor Szabłowski <iszablow@gmail.com>
2024-02-13 20:06:39 +0100

    App class

---
+++ src/main/java/application/App.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene studiesChooseScene = new Scene(new FXMLLoader(getClass().getResource("StudiesChoose.fxml")).load());
+        stage.setTitle("University notebook");
+        stage.setResizable(false);
+        stage.setScene(studiesChooseScene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
